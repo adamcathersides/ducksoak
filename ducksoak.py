b3e18d3f06001d1c9c0eadbdc4fd5522524e65a7
Adam Cathersides <adamcathersides@gmail.com>
2019-09-12 23:17:55 +0100

    Refactor

--- ducksoak.py
+++ ducksoak.py
@@ -7,7 +7,6 @@ import os
 import pwd
 import getpass
 import sys
-from time import sleep
 
 
 def parse_config(config_file):
@@ -22,6 +21,31 @@ def parse_config(config_file):
             return config
         except yaml.YAMLError as e:
             print(f'Erk that yaml is ugly: {e}')
+
+def start_analysis_containers(client, stream_list, image, logs, uid):
+
+    """
+    Start an analysis container per stream
+    """
+
+    for stream in stream_list:
+        print(f'Testing: {stream}')
+        mc_ip = stream.split(':')[0]
+        port = stream.split(':')[1]
+        # cmd = f'bash -c "tsp -I ip {mc_ip}:{port} -P continuity -O drop > {logs}/{mc_ip}.log"'
+        cmd = f'bash -c "while true; do sleep 1; echo thing; done > {logs}/{mc_ip}.log"'
+        print(cmd)
+        client.containers.run(
+                image,
+                detach=True,
+                command=cmd,
+                network_mode='host',
+                auto_remove=True,
+                name=mc_ip.replace('.', '-'),
+                tty=True,
+                volumes={logs: {'bind': logs, 'mode': 'rw'}},
+                user=uid
+                )
 
 
 def tidy_up(ctx, param, value):
@@ -57,26 +81,10 @@ def run(config):
         print('Log dir already exists')
     except OSError as e:
         print(f'Log dir creation error : {e}')
+        sys.exit(1)
 
-    for stream in cfg['inputs']:
-        print(f'Testing: {stream}')
-        mc_ip = stream.split(':')[0]
-        port = stream.split(':')[1]
-        # cmd = f'bash -c "tsp -I ip {mc_ip}:{port} -P continuity -O drop > {cfg["logs"]}/{mc_ip}.log"'
-        cmd = f'bash -c "while true; do sleep 1; echo thing; done > {cfg["logs"]}/{mc_ip}.log"'
-        print(cmd)
-        client.containers.run(
-                cfg['image'],
-                detach=True,
-                command=cmd,
-                network_mode='host',
-                auto_remove=True,
-                name=mc_ip.replace('.', '-'),
-                tty=True,
-                volumes={cfg['logs']: {'bind': cfg['logs'], 'mode': 'rw'}},
-                user=uid
-                )
-
+    start_analysis_containers(client, cfg['inputs'], cfg['image'], cfg['logs'], uid)
+    start_analysis_containers(client, cfg['outputs'], cfg['image'], cfg['logs'], uid)
 
 if __name__ == '__main__':
     run()
